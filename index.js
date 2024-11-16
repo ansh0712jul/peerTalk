3d04d25ed2ba6f3176d6bda80f3fc445084e4410
Ansh <anshagrawal181@gmail.com>
2024-11-16 15:27:34 +0530

    Updated logic and deleted unnecessary files

--- index.js
+++ index.js
@@ -6,23 +6,31 @@ const path = require("path");//--> path.join deta h
 const socketio = require("socket.io");
 const io=socketio(server);// its an object
 // for static files
-app.use('/',express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 
-
+app.get('/',(req,res)=>{
+    console.log("home page");
+    res.render("home")
+})
+app.get('/chat',(req,res)=>{
+    res.render("landing.ejs")
+})
 io.on("connection",(socket)=>{
     console.log( `connection is established at ${socket.id}`);
 
     socket.on("send-msg",(data)=>{// listen to some event
-        // console.log(data.msg)
-        io.emit("server-send-msg", {
+        console.log(data)
+        io.emit("recieved-msg", {
             msg: data.msg,
             id: socket.id
         });
     })
 })
 
-const port = process.env.PORT || 3000;
+const port =  3000;
 server.listen(port, () => {
     console.log(`server is listening on port ${port}`);
 })
--- public/script.js
+++ public/script.js
@@ -10,17 +10,21 @@ btn.addEventListener("click", () => {
     document.getElementById("inp").value="";
 })
 
-socket.on("server-send-msg", (data) => {
+socket.on("recieved-msg", (data) => {
+    // console.log(data.id)
     const li = document.createElement('li');
-    li.classList.add('border', 'mt-2', 'p-2', 'rounded-pill');
+    li.classList.add('border', 'mt-2', 'p-2', 'rounded-pill','w-50');
     const username="Anonymous"
-    if(data.id === socket.id){
-        li.style.backgroundColor = 'black';
-        li.style.color = 'white';
-    }else{
-        li.style.backgroundColor = 'green';
-        li.style.color = 'white'
+    if (data.id === socket.id) {
+        li.style.alignSelf = "flex-start"; // Align to the left for the same sender
+        li.style.backgroundColor = "#d1e7dd"; // Greenish background for same sender
+        li.style.color = "#1b4332";
+    } else {
+        li.style.alignSelf = "flex-end"; // Align to the right for a new sender
+        li.style.backgroundColor = "#f8d7da"; // Pinkish background for new sender
+        li.style.color = "#842029";
     }
+
     li.innerHTML = `<span class="fw-bold"> ${username} </span> -> ${data.msg} `;
     document.querySelector('#chat').append(li);
 
